84aaa5226ee6114f7d514adcb5b4013a3399ada2
KAJAL <113601134+iamKajal24@users.noreply.github.com>
2024-01-31 18:04:39 +0530

    Add files via upload

---
+++ TestThrowExam.java
@@ -0,0 +1,16 @@
+package ExceptionHandling;
+
+public class TestThrowExam {
+	public static void validate(int age) {
+		if (age < 18) {
+			throw new ArithmeticException("Person is not eligible to vote");
+		} else {
+			System.out.println("Person is eligible to vote!!");
+		}
+	}
+
+	public static void main(String[] args) {
+		validate(13);
+		System.out.println("Rest of the code");
+	}
+}
