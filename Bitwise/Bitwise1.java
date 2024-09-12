a8f616b62ad73cddcb5ee6f4682fc25e0c7c5ad4
KAJAL <113601134+iamKajal24@users.noreply.github.com>
2024-09-12 08:15:47 +0530

    Add files via upload

---
+++ Bitwise/Bitwise1.java
@@ -0,0 +1,17 @@
+package Bitwise;
+
+public class Bitwise1 {
+
+	public static void main(String[] args) {
+		int p=9;int q=10;
+		System.out.println(p|q);//11
+		System.out.println(p&q);//8
+		System.out.println(p^q);//3
+		
+		System.out.println(p<<1);//18
+		System.out.println(p<<2);//36
+		
+		System.out.println(q>>1);//5
+		System.out.println(q>>2);//2
+	}
+}
